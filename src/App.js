58b6b43fd5cd03378640dd0bd90390120242aa50
gregorymark254 <gregorymark254@gmail.com>
2023-05-30 13:40:27 +0300

    Added file pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import Header from './pages/Header'
 import Home from './pages/Home'
+import Nav from './pages/Nav'
+import Forums from './pages/Forums'
+import Notifications from './pages/Notification'
 import { Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Nav/>
       <Routes>
         <Route exact path="/" element={<Home/>} />
+        <Route exact path="/forums" element={<Forums/>} />
+        <Route exact path="/notifications" element={<Notifications/>} />
       </Routes>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import community from '../images/community.png'
 
 const Home = () => {
   return (
     <div>
-      home
+      <div className="container mx-auto p-4">
+        <div className="flex items-center justify-center">
+          <img width={600} height={600} src={community} alt="Community" />
+        </div>
+      </div>
     </div>
   )
 }
